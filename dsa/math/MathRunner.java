e577679a70534ba2960b718de2ddbae6cdec5197
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-04-04 07:58:35 +0530

    reverse a number and check for plaindom number

--- dsa/math/MathRunner.java
+++ dsa/math/MathRunner.java
@@ -3,9 +3,18 @@ package dsa.math;
 public class MathRunner {
     public static void main(String[] args)
     {
+
+        /*problme one to count digit in agiven number */
         NumberProbelms numberProbelms=new NumberProbelms();
         int numberToCount=12345671;
         System.out.print("Total digits:"+numberProbelms.totalDigitCount(numberToCount));
+        /* problem 2.reverse the given number  */
+        int numberToReverse=238787;
+        System.out.println("\nReverse of the"+numberToReverse+" is :"+numberProbelms.reverseNumber(numberToReverse));
+        /*Progarm to find a given number is palindrome */
+        int numberToCheckForPlaindrome=1221;
+        System.out.println(numberToCheckForPlaindrome+" is :"+numberProbelms.checkPlaindromeNumber(numberToCheckForPlaindrome));
+
 
 
     }
--- dsa/math/NumberProbelms.java
+++ dsa/math/NumberProbelms.java
@@ -18,4 +18,44 @@ public int totalDigitCount(int number)
     return totalCount;
 }
 
+/* Solution to Reverse the  Number */
+public int reverseNumber(int number)
+{
+     if(number == 0)
+     {
+        return 0;
+     }
+     int revNumber=0;
+     while(number> 0)
+     {
+        int lastDigit=number %10;
+        revNumber=revNumber*10 +lastDigit;
+        number/=10;
+     }
+     return revNumber;
+}
+
+public String checkPlaindromeNumber(int number)
+{
+    if(number == 0)
+    {
+        return "Not Palindrome";
+    }
+    int revNum=0;
+    int orignalNumber=number;
+    while(number > 0)
+    {
+        int lastDigit=number%10;
+        revNum=(revNum * 10) + lastDigit;
+        number/=10; 
+    }
+    if(revNum == orignalNumber)
+    {
+        return "Palindrome";
+    } else
+    {
+        return "Not Palindrome";
+    }
+}
+
 }

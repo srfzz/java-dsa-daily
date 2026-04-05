52bf6b988b0efa2207c25b9615155e8808aa556c
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-04-05 08:54:44 +0530

    added basic recurion question like Print N to 1 using Recursion,Sum of First N Numbers, Factorial of a given numberReverse an array,Check if String is Palindrome or Not,Fibonacci Number

--- dsa/basicrecursion/RecursionProbelms.java
+++ dsa/basicrecursion/RecursionProbelms.java
@@ -1,5 +1,7 @@
 package dsa.basicrecursion;
 
+import java.math.BigInteger;
+
 public class RecursionProbelms {
   static int count=0;;
     /*Solution Print name N times using recursion */
@@ -23,8 +25,79 @@ public class RecursionProbelms {
         {
             return ;
         }
-        System.out.print(current+",");
+        System.out.println(current+",");
         printNumber(current+1, target);
     }
 
+
+
+    public void nToOne(int number)
+    {
+        if(number ==0)
+        {
+            return ;
+        }
+        System.out.println("\n"+number);
+       nToOne(number-1);
+    }
+   public int sumOfFisrtnNumbers(int number)
+   {
+        
+        if(number <=0)
+        {
+            return 0;
+        }
+        return number + sumOfFisrtnNumbers(number-1);
+
+   }
+
+   public BigInteger factorialOfNumber(int number)
+   {
+    if( number <=1 )
+    {
+        return BigInteger.ONE;
+    }
+    return BigInteger.valueOf(number).multiply( factorialOfNumber(number-1));
+   }
+
+   //* solution to reverse an array */
+
+   public void reverseArray(int[] number,int begin,int end)
+   {
+    if(begin >=end)
+    {
+        return ;
+    }
+    int temp=number[begin];
+    number[begin]=number[end];
+    number[end]=temp;
+    reverseArray(number, begin+1, end-1);
+   }
+
+   public boolean isStringPalindrome(char[] arr,int begin,int end)
+   {
+    if(begin >= end)
+    {
+        return true;
+    }
+    if(arr[begin]!=arr[end])
+    {
+             return false;
+    }
+     return  isStringPalindrome(arr, begin+1, end-1);
+   }
+   /*solution for fibonncai number using recusrsion */
+   public void fibonaccaiSeriesUptoN(int num)
+   {
+      generatefibonaccaiSeries(num,0,1);
+   }
+   private void generatefibonaccaiSeries(int num,int f1,int f2)
+   {
+    if(num <=0)
+    {
+        return ;
+    }
+    System.out.print(f1+",");
+    generatefibonaccaiSeries(num-1, f2, f1+f2);
+   }
 }
--- dsa/basicrecursion/RecursionRunner.java
+++ dsa/basicrecursion/RecursionRunner.java
@@ -1,5 +1,7 @@
 package dsa.basicrecursion;
 
+import java.util.Arrays;
+
 public class RecursionRunner {
     public static void main(String[] args)
     {
@@ -8,8 +10,35 @@ public class RecursionRunner {
         String myName="Sarfaraj";
         int times=5;
         rec.printName(myName,times);
+
         /* Print 1 to N using Recursion */
         rec.printOneToN(56);
+
+        /*weap to print n ot 1 using recursion whwre n is the number  */
+        rec.nToOne(10);
+
+        /* Sum of First N Numbers */
+        System.out.println("Sum of first n number:"+rec.sumOfFisrtnNumbers(5));
+
+        /* Factorial of a given number */
+
+        System.out.printf("factorial of the number is :%d \n",rec.factorialOfNumber(10));
+
+        /*Reverse an array */
+       int[] numbers={1,34,87,22,78,07};
+       rec.reverseArray(numbers, 0, numbers.length-1);
+       System.out.println(Arrays.toString(numbers));
+
+
+       /*Check if String is Palindrome or Not */
+        String name ="abbta";
+        char[] nameArray=name.toCharArray();
+        System.out.println(rec.isStringPalindrome(nameArray, 0, nameArray.length-1));
+
+
+        /* Fibonacci Number */
+        int fibLimit=10;
+        rec.fibonaccaiSeriesUptoN(fibLimit);
 
     }
 

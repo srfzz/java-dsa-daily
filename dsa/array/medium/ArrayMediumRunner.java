8e3c2b41c9d6e9a3a4d9536491cc743bfa1d1988
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-04-20 08:41:56 +0530

    added sor os one and tws

--- dsa/array/medium/ArrayMediumRunner.java
+++ dsa/array/medium/ArrayMediumRunner.java
@@ -9,6 +9,18 @@ public class ArrayMediumRunner {
     int[] twoSum={2,6,5,8,11};
     ArrayMediumSolution sol=new ArrayMediumSolution();
     System.out.println(sol.sumEqualsTotarget(twoSum, 14));
+
+
+    /*
+    Sort an array of 0's 1's and 2's
+    Input: nums = [1, 0, 2, 1, 0]
+    Output: [0, 0, 1, 1, 2]
+    Explanation: The nums array in sorted order has 2 zeroes, 2 ones and 1 two
+
+    */
+
+    int[] nums={1, 0, 2, 1, 0};
+    sol.sortArrayInAscWithoutCreatingNewArray(nums);
   }
 
 
--- dsa/array/medium/ArrayMediumSolution.java
+++ dsa/array/medium/ArrayMediumSolution.java
@@ -25,4 +25,39 @@ public String sumEqualsTotarget(int[] arr,int target){
     /*Two Sum : Check if a pair with given sum exists in Array */
     /*1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO. */
 
+
+    
+    /*
+    Sort an array of 0's 1's and 2's
+    Input: nums = [1, 0, 2, 1, 0]
+    Output: [0, 0, 1, 1, 2]
+    Explanation: The nums array in sorted order has 2 zeroes, 2 ones and 1 two
+
+    */
+
+    public void sortArrayInAscWithoutCreatingNewArray(int[] nums){
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+
+            }
+            if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp = nums[high];
+            nums[high] = nums[mid];
+            nums[mid] = temp;
+            high--;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
 }
